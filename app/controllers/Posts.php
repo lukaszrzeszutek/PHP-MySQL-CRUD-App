a53c44e7b6a6cadc6b3e6a25d5ac41b11989fe13
lukaszrzeszutek <lucas.rzeszut@gmail.com>
2018-09-19 23:00:15 +0200

    Inserting Posts

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -20,11 +20,51 @@ class Posts extends Controller {
   }
 
   public function add(){
-    $data = [
-      'title' => '',
-      'body' => ''
-    ];
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      // Sanitize POST data
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-    $this->view('posts/add', $data);
+      // Init data
+      $data =[
+        'title' => trim($_POST['title']),
+        'body' => trim($_POST['body']),
+        'user_id' => $_SESSION['user_id'],
+        'title_err' => '',
+        'body_err' => ''
+      ];
+
+      // Validate title
+      if(empty($data['title'])){
+        $data['title_err'] = 'Please enter title';
+      }
+
+      // Validate body
+      if(empty($data['body'])){
+        $data['body_err'] = 'Please enter body text';
+      }
+
+      // Make sure no errors
+      if(empty($data['title_err']) && empty($data['body_err'])){
+        //Validated
+        if($this->postModel->addPost($data)){
+          flash('post_message', 'Post Added');
+          redirect('Something went wrong');
+        } else {
+          die('Something went wrong');
+        }
+      } else {
+        // Load view with errors
+        $this->view('posts/add', $data);
+      }
+
+    } else {
+      $data = [
+        'title' => '',
+        'body' => ''
+      ];
+
+      $this->view('posts/add', $data);
+    }
+
   }
 }
